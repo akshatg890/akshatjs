42840132c14a9fe8624a0d34b179bff42cea882c
akshatg890 <143016829+akshatg890@users.noreply.github.com>
2024-09-18 08:58:13 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,5 @@
+const name="Akshat"
+const repo="80"
+console.log(name + repo + "value")
+//another way
+console.log(`Hello my name is ${name} and my repo count is ${repo}`)
---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,19 @@
+//two datatypes 1. primitive 2. non-primtive
+//primitive - 7 types String, Number, boolean, null , undefined, symbol, undefined
+// non-primtve
+//aaray,object,functions
+
+const big=1234658353738n
+console.log(typeof big)
+//object
+let myObj={
+    name: "Akshat",
+    age: 20
+}
+function myfunction(){
+    console.log("hello to the akshat")
+}
+myfunction()
+// array
+const hero=["spider","antman"]
+console.log(hero)

151e42f5bce263ba076df44f34a16189a4bdf6f9
akshatg890 <143016829+akshatg890@users.noreply.github.com>
2024-09-18 17:56:33 +0000

    string

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -3,3 +3,23 @@ const repo="80"
 console.log(name + repo + "value")
 //another way
 console.log(`Hello my name is ${name} and my repo count is ${repo}`)
+const  n= new String("AkshatKumar")
+console.log(n)
+console.log(n.charAt(2))
+console.log(n.indexOf("K"))
+const nw=n.substring(0,6)
+console.log(n.length);
+console.log(nw)
+const nw1=n.slice(-10,4)
+console.log(nw1)
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));

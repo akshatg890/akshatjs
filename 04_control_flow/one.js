58cd75ffdb66e6a44d5c8d6d3c8adef2865b765d
akshatg890 <143016829+akshatg890@users.noreply.github.com>
2024-09-25 18:02:04 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,58 @@
+if(true){
+    console.log("hello");
+    
+}
+if(2==="3"){
+    console.log("excuted");
+    
+}
+const balance=900
+if(balance>800) console.log("yes"), console.log("no");
+const isUserloggedIn = true
+const temperature = 41
+
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+const debitcard=true
+const creditcard=false
+if(creditcard && debitcard && 2==2){
+    console.log("Akshat is good")
+}
+if(creditcard || debitcard){
+    console.log("NO");
+    
+}
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,15 @@
+const month="may"
+switch(month){
+    case "jan":
+        console.log("jan");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "may":
+        console.log("may");
+        break;
+}
